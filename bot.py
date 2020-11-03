ca3546819d6bcff7f434d1f1bc20f266d0fadd44
pxnx <loool1912@web.de>
2020-11-03 14:57:50 +0100

    Added staff

--- bot.py
+++ bot.py
@@ -47,9 +47,9 @@ def admins(update, context):
 
     text = "*Group's staff*" \
            "\n\nOrganization" \
-           "\n@aakaah00001" \
-           "\n@Prashant_Choudhary" \
-        #  "\n@PacificPC" \
+        #    "\n@aakaah00001" \
+    #   "\n@Prashant_Choudhary" \
+    #  "\n@PacificPC" \
     #    "\n\nModerators" \
     #  "\n@pentexnyx" \
     #  "\n@Abhishek2376"
