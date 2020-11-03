3f20c4df357953a805748e49489b9e3189d27890
pxnx <loool1912@web.de>
2020-11-03 22:24:15 +0100

    Added staff

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def start(update, context):
 
 
 def button(update: Update, context: CallbackContext) -> None:
-    text: str
+    message_text: str
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
@@ -48,8 +48,8 @@ def button(update: Update, context: CallbackContext) -> None:
     button_text = "Proceed ➡"
 
     if position == '0':
-        text = "Which device are you using?" \
-               "\n\nWhich software update is installed?"
+        message_text = "Which device are you using?" \
+                       "\n\nWhich software update is installed?"
     #   elif position == 1:
     #       text = "What do you want do?" \
     #             "\n\nWhat have you tried already?"
@@ -60,8 +60,8 @@ def button(update: Update, context: CallbackContext) -> None:
     #      text = "What output did you get?"
     #   else:
     #       text = "That's been it. Thanks for your time. 🤖" \
-    ##              "\nI will inform our experts now."
-    ##       button_text = "Submit answers"
+    #              "\nI will inform our experts now."
+    #       button_text = "Submit answers"
 
     position += 1
     proceed_button = InlineKeyboardMarkup.from_button(InlineKeyboardButton(button_text, callback_data=str(position)))
