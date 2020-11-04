566a9e05e94406d4f382dc4da1ed92537e72ea1a
pxnx <loool1912@web.de>
2020-11-04 19:34:32 +0100

    Private chat

--- bot.py
+++ bot.py
@@ -267,7 +267,7 @@ def when_update(update, context):
     replied_message = update.message.message_id
 
     chat_id = update.message.chat_id
-    context.job_queue.run_once(alarm,
+    context.job_queue.run_once(context.bot.delete_message(chat_id=-1001327617858, message_id=update.message.message_id),
                                10,
                                context=chat_id, name=str(update.message.message_id))
 
