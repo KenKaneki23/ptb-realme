67602699a93314daded87c942eecc605d19751ed
pxnx <loool1912@web.de>
2020-11-11 16:24:25 +0100

    Private chat

--- bot.py
+++ bot.py
@@ -158,7 +158,7 @@ def rules(update, context):
 
 
 def new_member_join(update: Update, context: CallbackContext):
-    delay_group(update, context, "New Member!")
+    update.message.reply_text("New Member!!!!")
 
 
 def form(update, context):
