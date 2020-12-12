143859a52e488b538180e9c97ae2eacc4abc7e9b
pxnx <loool1912@web.de>
2020-12-12 20:31:15 +0100

    auto delete message fix

--- bot.py
+++ bot.py
@@ -418,6 +418,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
     updater.bot.setWebhook('https://ptb-realme.herokuapp.com/' + TOKEN)
 
+    updater.start_polling()
     updater.idle()
 
 
