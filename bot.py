142c47592e70fda6a4162c948c5270b2d9644448
pxnx <loool1912@web.de>
2021-01-05 12:37:46 +0100

    hopefully this time it keeps on working :D

--- bot.py
+++ bot.py
@@ -264,8 +264,8 @@ def main():
 
     # dp.add_error_handler(error)
 
-    #  updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    ##   updater.bot.setWebhook('https://ptb-realme.herokuapp.com/' + TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.setWebhook('https://ptb-realme.herokuapp.com/' + TOKEN)
 
     updater.start_polling()
     updater.idle()
