6969bda1eed0dcde98bdaab2da80e77325cbecd5
pxnx <loool1912@web.de>
2021-01-07 17:44:34 +0100

    if update.message.reply_to_message is not None now.. let's see if it crashes again :)

--- vvvv.py
+++ vvvv.py
@@ -1,9 +1,10 @@
 import os
-import telegram
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
 
-TOKEN = "TOKEN"
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+TOKEN = "1415969330:AAGEnSGxjYl-hd3VTkpS4uY017Wag5dDsDQ"
 
 
 def start(update: Update, context: CallbackContext):
@@ -11,8 +12,9 @@ def start(update: Update, context: CallbackContext):
 
 
 def delay_group(update: Update, context: CallbackContext, text: str):
+
     if update.effective_message.reply_to_message:
-        update.message.reply_to_message.reply_text(
+        update.effective_message.reply_to_message.reply_text(
             text=text,
             parse_mode=telegram.ParseMode.HTML)
     else:
@@ -22,14 +24,15 @@ def delay_group(update: Update, context: CallbackContext, text: str):
             parse_mode=telegram.ParseMode.HTML)
         context.job_queue.run_once(delete, 30, context=reply_message.chat_id, name=str(reply_message.message_id))
 
-    update.message.delete()
+    update.effective_message.delete()
+
 
 
 def delete(context: CallbackContext):
     telegram.Message = context.bot.delete_message(chat_id=str(context.job.context), message_id=context.job.name)
 
 
-if __name__ == '__main__':
+def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("test", start))
@@ -37,7 +40,11 @@ if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 5000))
 
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.setWebhook('https://app-name.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://ptb-realme.herokuapp.com/' + TOKEN)
 
     updater.start_polling()
     updater.idle()
+
+
+if __name__ == '__main__':
+    main()
