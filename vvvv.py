ff1f2949010c4cea390d3fdf3991eada44d3d59d
pxnx <loool1912@web.de>
2021-01-07 16:33:01 +0100

    if update.message.reply_to_message is not None now.. let's see if it crashes again :)

---
+++ vvvv.py
@@ -0,0 +1,51 @@
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+TOKEN = "1415969330:AAGEnSGxjYl-hd3VTkpS4uY017Wag5dDsDQ"
+
+def start(update: Update, context: CallbackContext):
+    delay_group(update, context, "Fancy text")
+    message_html(update, context, "Even fancier text")
+
+
+def delay_group(update: Update, context: CallbackContext, text: str):
+    update.message.delete()
+
+    if update.message.reply_to_message:
+        update.message.reply_to_message.reply_text(
+            text=text,
+            parse_mode=telegram.ParseMode.HTML)
+    else:
+        reply_message = context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=text,
+            parse_mode=telegram.ParseMode.HTML)
+        context.job_queue.run_once(delete, 600, context=reply_message.chat_id, name=str(reply_message.message_id))
+
+
+def message_html(update: Update, context: CallbackContext, text):  # return context.bot.send_message(
+    if update.message.reply_to_message:
+        return update.message.reply_to_message.reply_text(
+            text=text,
+            parse_mode=telegram.ParseMode.HTML)
+    else:
+        return context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=text,
+            parse_mode=telegram.ParseMode.HTML)
+
+def delete(context: CallbackContext):
+    telegram.Message = context.bot.delete_message(chat_id=str(context.job.context), message_id=context.job.name)
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("test", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
