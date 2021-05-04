440d3ab2bf606641079f6c1244ff439ccdf29d2a
pentexnyx <loool1912@web.de>
2021-05-04 23:03:19 +0200

    postgre - good luck

--- messages.py
+++ messages.py
@@ -260,7 +260,7 @@ def polls(update: Update, context: CallbackContext):  # GROUP
 
      #   context.chat_data["polls_previous_date"] = now()
         print("inserrrrt")
-        cur.execute("CREATE TABLE bot_data")
+        cur.execute("CREATE TABLE bot_data (previous_link varchar, previous_timestamp integer)")
         cur.execute(f'INSERT INTO bot_data ({now()}, {msg})')
 
         print(cur.execute('SELECT * FROM bot_data'))
