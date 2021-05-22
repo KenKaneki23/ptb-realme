397caff46aa713eedde9533ddf80c7a8e73dfd8a
pentexnyx <loool1912@web.de>
2021-05-22 11:52:44 +0200

    trying to fix postgres again....

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ from utils import remove_message
 ##########################################
 
 PORT = int(os.environ.get('PORT', 5000))
-DATABASE_URL = "postgresql://blpxjuiwemaqpm:4c9a03691098bb7a6f615d510652c7c355b030da52e24b2cb48653a145082205@ec2-54" \
-               "-220-35-19.eu-west-1.compute.amazonaws.com:5432/dcu1jgmr7u3uv3 "
+DATABASE_URL = os.environ['DATABASE_URL'].replace('postgres', 'postgresql',1)
+
 TOKEN = os.environ['TOKEN']
 
 OFFTOPIC_GROUP = -1001415779011
 GROUP = -1001374176745  # -1001327617858 for test group
 
 ADMINS = (703453307,  # Nyx
-          984010225  # Phoenix
+          984010225  # Phoenix #TODO replace with BlueBettle
           )
 VERIFIED_USERS = ADMINS + (
-    92429516,  # Lucky
+    924295169,  # Lucky
     1038099761  # Abhiskek
 )
 
